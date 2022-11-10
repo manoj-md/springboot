66e8a1b51ff8efcf053d5c00d2b0e883eb1cf053
Manoj D <manojd@Manojs-MacBook-Air.local>
2022-11-10 23:31:15 +0530

    Added a spring boot application

---
+++ hello/src/main/java/com/manoj/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.manoj.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "Hello Welcome to my first springboot application ";
+    }
+}
+  
